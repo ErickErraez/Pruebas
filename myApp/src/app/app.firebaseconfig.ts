0047b9aae690092a6254185bf384e556fcd0fb56
ErickErraez <edg.erraez@yavirac.edu.ec>
2018-10-18 11:19:07 -0500

    MyNewApp

---
+++ myApp/src/app/app.firebaseconfig.ts
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyBY8hKyelzenloXFG6de56yKFS4UZHli4U",
+  authDomain: "yavzinger.firebaseapp.com",
+  databaseURL: "https://yavzinger.firebaseio.com",
+  projectId: "yavzinger",
+  storageBucket: "yavzinger.appspot.com",
+  messagingSenderId: "14622928629"
+};
